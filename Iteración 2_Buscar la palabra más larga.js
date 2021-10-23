a011b30d7ade18be373c8cfbbffd187c30cce79a
Soy Adrián Torres <adriat1994@gmail.com>
2021-10-23 17:30:18 +0200

    Iteración 2-Comentarios

--- Iteración 2_Buscar la palabra más larga.js
+++ Iteración 2_Buscar la palabra más larga.js
@@ -1,11 +1,11 @@
 console.log(longestStringForLoop(['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.']));
 
 function longestStringForLoop(arr) {
-  let word = "";
+  let word = ""; //Esta variable sirve para ir almacenando y comparando cada string, cada nombre de heroe en cada vuelta
   for (let i = 0; i < arr.length; i++) {
-    if (word.length < arr[i].length) {
-      word = arr[i];
-    }
+    if (word.length < arr[i].length) { //Aquí indicamos que si el string de nuestro array que está en el bucle ahora mismo tiene más longitud que la variable word, ese string que está en el bucle pasa a ser el valor de la variable word
+      word = arr[i];  
+    } //Esto sirve para que se vayan comparando la longuitud de cada string entre sí, para que al final del bucle la variable word sea el string más largo de nuestro array
   }
   return word;
 }
